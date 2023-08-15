e39bcd2236c1708d41e3e7429505220daf6b45df
aadarshksingh <aadarshkumarsingh198@gmail.com>
2023-08-15 15:40:01 +0530

    Initial Commit

---
+++ src/main/java/com/manage/libros/AdminController.java
@@ -0,0 +1,4 @@
+package com.manage.libros;
+
+public class AdminController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.manage.libros.libros {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+//    requires eu.hansolo.tilesfx;
+
+    opens com.manage.libros to javafx.fxml;
+    exports com.manage.libros;
+}
